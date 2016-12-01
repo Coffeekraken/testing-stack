8db40daa67149c98edf53d915ed3987fe1a0492b
Olivier Bossel <olivier.bossel@gmail.com>
2016-12-01 16:06:03 +0100

    adding webpack to generate dist files

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+		entry: {
+			index : './index.js'
+		},
+		output: {
+				path: './dist',
+				filename: '[name].js',
+		},
+		module: {
+				loaders: [{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+				}]
+		}
+}
