0e031af480fe5738f440ec8852f22268ad6b4e3b
Olivier Bossel <olivier.bossel@gmail.com>
2016-12-01 17:56:34 +0100

    up

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import __mochaApi from './mocha/index';
 import __setup from './setup';
-
 const api = {
 	setup : __setup,
 	mocha : __mochaApi
--- setup.js
+++ setup.js
@@ -1,6 +1,5 @@
 import __settings from './settings';
 import __extend from 'lodash/extend';
-
 export default function setup(settings) {
 	__extend(__settings, settings);
 }
