39c54919ea16e5373dd90ad58d010be218c5b802
Olivier Bossel <olivier.bossel@gmail.com>
2016-12-01 15:49:29 +0100

    up

--- src/setup.js
+++ src/setup.js
@@ -2,5 +2,5 @@ import __settings from './settings';
 import __lodash from 'lodash';
 
 export default function setup(settings) {
-	__settings = __lodash.extend(__settings, settings);
+	__lodash.extend(__settings, settings);
 }
